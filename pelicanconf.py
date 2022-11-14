0595a48377713f03e6584f7ec0005110d4727c5b
L-S <rkbxlq62a@relay.firefox.com>
2022-11-14 08:03:07 -0500

    Delete ignored files

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,20 @@
-AUTHOR = 'Louis-Simon Mc Nicoll'
-SITENAME = 'Pense-bête'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Louis-Simon Mc Nicoll'
+SITENAME = u'Pense-bête'
+SITESUBTITLE = u'Blog description here.'
 SITEURL = ''
 
 PATH = 'content'
+OUTPUT_PATH = 'docs' # Github pages' root
 
+DEFAULT_DATE = 'fs'
+DEFAULT_DATE_FORMAT = '%d %b %Y'
 TIMEZONE = 'America/Toronto'
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -15,19 +23,56 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-# LINKS = (('Pelican', 'https://getpelican.com/'),
-#          ('Python.org', 'https://www.python.org/'),
-#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-#          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-        #   ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
+# Pagination
+DEFAULT_PAGINATION = 3
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME='blue-penguin'
+# Post and Pages path
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+# Tags and Category path
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'catgegories.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+
+# Author
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
+
+# To show the line numbers for code blocks
+# Refer https://docs.getpelican.com/en/stable/settings.html?highlight=MARKDOWN#basic-settings
+MARKDOWN = {
+  'extension_configs': {
+    'markdown.extensions.codehilite': {'css_class': 'highlight', 'linenums': True},
+    'markdown.extensions.extra': {},
+    'markdown.extensions.meta': {},
+  },
+  'output_format': 'html5',
+}
+PYGMENTS_RST_OPTIONS = {'linenos': 'table'}
+
+### Plugins
+
+PLUGIN_PATHS = [
+  'pelican-plugins'
+]
+
+THEME = 'attila'
+
+### Theme specific settings
+
--- publishconf.py
+++ publishconf.py
@@ -7,10 +7,4 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
-RELATIVE_URLS = False
-
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
-
-DELETE_OUTPUT_DIRECTORY = True
+SITEURL = 'https://blog.louissimonmcnicoll.com'
